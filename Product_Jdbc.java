973e338dab83489dca5de43ea17301bd9ca90a6b
samrudhi27 <76120226+samrudhi27@users.noreply.github.com>
2021-08-10 16:04:39 +0530

    Add files via upload

--- Product_Jdbc.java
+++ Product_Jdbc.java
@@ -2,6 +2,7 @@ package com.telstra.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,16 +13,26 @@ public class Product_Jdbc {
 		String username="root";
 		String password="root";
 		//String createtable="create table product1(prodId int(4), prodName varchar(20), prodPrice int(5), inStock varchar(4));";
-		String updateCommand="update product SET prodName='lg washing machine' where prodId=2;";
+		//String updateCommand="update product SET prodName='lg washing machine' where prodId=2;";
 		//String insertCommand="insert into product values(2,'lg tv',35000,'yes');";
+		String selectCommand="select * from product";
 		int i=0;
 		Connection cn=null;
 		Statement st=null;
+		ResultSet rs=null;
 		cn=DriverManager.getConnection(url, username, password);
 		st=cn.createStatement();
 		//i=st.executeUpdate(createtable);
-		i=st.executeUpdate(updateCommand);
-		System.out.println(i+" No of records added.");
+		rs=st.executeQuery(selectCommand);
+		System.out.println("prodId\t prodName\t prodPrice\t inStock");
+		while(rs.next()) {
+			int prodid=rs.getInt("prodId");
+			String prodname=rs.getString("prodName");
+			String prodprice=rs.getString("prodPrice");
+			String instock=rs.getString("inStock");
+			System.out.println(prodid+":\t"+prodname+":\t"+prodprice+":\t"+instock);
+		}
+		//System.out.println(i+" No of records added.");
 
 	}
 
